b51ac49309e5d3db6b4ad08bc9514d93f7606c8f
Jean-Daniel Liwanga <jeandaniel.liwanga@gmail.com>
2024-01-17 17:40:11 -0500

    Built the frontend, unto creating a site for it

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 import { BrowserRouter } from "react-router-dom";
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
